d44adeefbbce44313f39a33b9ec561d2eba7d5f7
Brandon Skewes <brandon.skewes@outlook.com>
2025-05-17 20:15:17 +1000

    remote repo

--- working_directory/01_two_sum.py
+++
@@ -1,9 +0,0 @@
-def two_sum(a,b):
-    """
-    This function should return the sum of two integers. None should be considerd as 0.
-
-    Example:
-        >>> two_sum(4,5)
-        >>> 9
-    """
-    return a+b
